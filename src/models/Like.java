8b32eb7e93a431e96f08bfccd0d6ff0e7e751cd0
yuta.arai <yutaarairai@icloud.com>
2019-11-30 16:14:30 +0900

    Add Employee class

---
+++ src/models/Like.java
@@ -0,0 +1,78 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "likes")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllLikes",
+            query = "SELECT l FROM Employee AS l ORDER BY l.id DESC"
+            ),
+    @NamedQuery(
+            name = "getLikesCount",
+            query = "SELECT COUNT(l) FROM Employee AS l"
+            ),
+})
+@Entity
+public class Like {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "report_id", nullable = false)
+    private Report report;
+
+    @ManyToOne
+    @JoinColumn(name = "like_employee_id", nullable = false)
+    private Employee employee;
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Report getReport() {
+        return report;
+    }
+
+    public void setReport(Report report) {
+        this.report = report;
+    }
+
+        public Employee getEmployee() {
+            return employee;
+        }
+
+        public void setEmployee(Employee employee) {
+            this.employee = employee;
+        }
+
+        public Timestamp getUpdated_at() {
+            return updated_at;
+        }
+
+        public void setUpdated_at(Timestamp updated_at) {
+            this.updated_at = updated_at;
+        }
+
+}
